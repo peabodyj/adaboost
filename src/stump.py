a2743f75836cde4d0c8ef5c6eaec88288dfc1bd2
Justin Peabody <preabody@gmail.com>
2017-12-06 00:15:30 -0600

    Cleaned up stump

--- src/stump.py
+++ src/stump.py
@@ -1,25 +1,33 @@
-import sys
+#
+# Stump - Used in AdaBoost as a weak learner. Splits data based on a single
+#         feature and split value. AKA not quite a tree. One side of the
+#         split will predict a positive label and one side will predict a
+#         negative label.
+#
 import numpy as np
 
 
 class Stump(object):
+
 	def __init__(self, info_gain, dimension, split_value):
+		# Save the info gain level to help us find the best split easier
 		self._info_gain = info_gain
+		# The details of our weak learner
 		self._dimension = dimension
 		self._split_value = split_value
 
-	def is_info_gain_better(self, info_gain):
+	def update_info_gain_if_better(self, info_gain, dimension, split_value, X, y, w):
+		# Use this function to grab the best stump possible
 		if info_gain > self._info_gain:
-			return True
-		return False
-
-	def update_info_gain(self, info_gain, dimension, split_value, X, y, w):
-		self._info_gain = info_gain
-		self._dimension = dimension
-		self._split_value = split_value
-		(self._left_sign, self._right_sign) = self.find_signs(X, y, w)
+			self._info_gain = info_gain
+			self._dimension = dimension
+			self._split_value = split_value
+			(self._left_sign, self._right_sign) = self.find_signs(X, y, w)
 
 	def find_signs(self, X, y, w):
+		# "Left" and "Right" just signify each side of our split
+		# Find what the majority label is for each side to determine what we
+		# will predict
 		y_left = y[np.where(X[:,self._dimension] <= self._split_value)]
 		w_left = w[np.where(X[:,self._dimension] <= self._split_value)]
 		neg_left_sum = np.sum(w_left[np.where(y_left == -1)])
@@ -32,6 +40,7 @@ class Stump(object):
 		pos_right_sum = np.sum(w_right[np.where(y_right == 1)])
 		w_right_sum = pos_right_sum - neg_right_sum
 
+		# Return as +1/0/-1 value to make error calclation much easier later
 		return (np.sign(w_left_sum), np.sign(w_right_sum))
 
 	def calculate_error(self, X, y, w):
@@ -39,6 +48,8 @@ class Stump(object):
 
 		predictions = np.zeros(n)
 		error = 0
+		# Keep a running total for error for each mispredicted label.
+		# TODO: Can probably numpy.vectorize() this if we need slight speed ups
 		for i in xrange(n):
 			if X[i,self._dimension] <= self._split_value:
 				predictions[i] = self._left_sign
