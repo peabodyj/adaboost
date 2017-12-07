a5b5c70b782ab647fa962a381270b0a6078c4f04
Justin Peabody <preabody@gmail.com>
2017-12-06 20:29:18 -0600

    Allow simple command line arguments

--- src/run.py
+++ src/run.py
@@ -1,7 +1,22 @@
 #!/usr/bin/env python
 
+import sys
 from adaboost import AdaBoost
 
-ada = AdaBoost("./zip.train",
-			   "./zip.test")
+# defaults
+stumps = 50
+train = "../data/zip.train"
+test = "../data/zip.test"
+pos_lbl = 1
+neg_lbl = 2
+
+if len(sys.argv) > 5:
+	stumps = int(sys.argv[1])
+	train = sys.argv[2]
+	test = sys.argv[3]
+	pos_lbl = int(sys.argv[4])
+	neg_lbl = int(sys.argv[5])
+
+
+ada = AdaBoost(stumps, train, test, pos_lbl, neg_lbl)
 ada.run()
